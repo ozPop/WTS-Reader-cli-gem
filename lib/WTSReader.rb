428cb9eb4b6fa6bfbb331a2222e313c46b57bc12
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-16 11:24:14 -0400

    added default settings for pushing text to say

--- lib/WTSReader.rb
+++ lib/WTSReader.rb
@@ -4,12 +4,20 @@ require "open-uri"
 
 module WTSReader
   class Reader
-    def initialize(url, rate, voice, path, ext)
+    # every instance of Reader can only have one document
+    attr_reader :doc
+    # `say` settings can be changed dynamically
+    attr_accessor :rate, :voice, :path
+    # default values are set for temporary files with default OSX voice at default rate (in word-per-minute)
+    def initialize(url, rate=205, voice='Alex', path='/tmp/', ext='.aac')
       @doc = Nokogiri::HTML(open(url))
       @rate = rate
       @voice = voice
       @path = path
       @ext = ext
+    end
+    def text
+      @doc.text
     end
   end
 end
