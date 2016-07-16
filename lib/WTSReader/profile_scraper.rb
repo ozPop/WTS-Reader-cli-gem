d3f14f5ffced87de855920904e864840fca57048
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-16 15:14:36 -0400

    finished working parser for TheGuardian articles

--- lib/WTSReader/profile_scraper.rb
+++ lib/WTSReader/profile_scraper.rb
@@ -1,13 +1,20 @@
 module WTSReader
   class Profiles
-    def match?
+    def match?(url)
+      if url.match(/theguardian.com/) || url.match(/theguardian.co.uk/)
+	get_guardian_text(
+      end
     end
-    def guardian_profile(doc)
-      headline = doc.css("h1.content__headline")
-      blurb = doc.css("div.content__standfirst p")
-      author = doc.css("p.byline")
-      date = doc.css("p.content__dateline")
-      content = doc.css("div.content__article-body p")
+    def guardian_parse(doc)
+      @headline = doc.css("h1.content__headline").text
+      @blurb = doc.css("div.content__standfirst p").text
+      @author = doc.css("p.byline").text
+      @date = doc.css("p.content__dateline").text
+      @content = doc.css("div.content__article-body p").text
+    end
+    def get_guardian_text(doc)
+      guardian_parse(doc)
+      "#{@headline}\n\nBy #{@author}\n\n#{@blurb}\nPublication date: #{@date}\n#{@content}"
     end
   end
 end
